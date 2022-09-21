fc8fb61d1507b0ab71f0920f97a696eababccefb
omarenis <omar.triki@enis.tn>
2022-09-21 23:43:12 +0100

    the final project

--- src/main/java/com/med_ali/invoice_generator/controllers/InvoiceGeneratorController.java
+++ src/main/java/com/med_ali/invoice_generator/controllers/InvoiceGeneratorController.java
@@ -3,14 +3,21 @@ package com.med_ali.invoice_generator.controllers;
 import com.med_ali.invoice_generator.models.Command;
 import com.med_ali.invoice_generator.service.InvoiceGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController
 public class InvoiceGeneratorController {
@@ -23,9 +30,19 @@ public class InvoiceGeneratorController {
     }
 
     @PostMapping("/invoice/generate")
-    public byte[] generateInvoice(@RequestBody Command command) throws IOException {
-        byte[] mybytes = new byte[20];
-        invoiceGenerator.generatePdf(command).write(mybytes);
-        return mybytes;
+    public ResponseEntity<Resource> generateInvoice(@RequestBody Command command) throws IOException {
+        invoiceGenerator.generatePdf(command);
+        Path path = Paths.get("./model.pdf");
+        Resource resource = null;
+        try {
+            resource = new UrlResource(path.toUri());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        assert resource != null;
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(MediaType.APPLICATION_PDF_VALUE))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
     }
 }
